347e108d445270ecfab497fb00582a7bee2496f0
unknown <arturaz.dg@Gmail.com>
2022-02-04 08:44:18 +0200

    added eventlistener

--- code/script.js
+++ code/script.js
@@ -1,5 +1,10 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const inputWrapper = document.getElementById("input-wrapper");
+const form = document.getElementById("name-form");
+const userInputValue = document.getElementById("name-input").value;
+
+let step = 1;
 
 // Global variables, if you need any, declared here
 
@@ -27,21 +32,28 @@ const showMessage = (message, sender) => {
     `
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+  chat.scrollTop = chat.scrollHeight;
 }
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there, What's your name?`, 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 // Set up your eventlisteners here
+form.addEventListener("submit",(event) => {
+  event.preventDefault();
 
+  if (Number(userInputValue) === 10) {
+    showMessage(`${userInputValue}`, 'user');
+    console.log ("hello")
+  }
+});
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
 // greeting()
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 1000);
