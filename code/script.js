dc71fdf03e05735aa069c7dbb4c524685ab7326f
unknown <arturaz.dg@Gmail.com>
2022-02-13 12:57:40 +0200

    added second question and its reply

--- code/script.js
+++ code/script.js
@@ -4,8 +4,10 @@ const inputWrapper = document.getElementById("input-wrapper");
 const form = document.getElementById("name-form");
 const nameInput = document.getElementById("name-input");
 // Global variables, if you need any, declared here
-let question = 1;
+let question = 0;
 // Functions declared here
+
+//These two functions are used for displaying replies as user or as a bot
 const userReply = () => {
   showMessage(`${nameInput.value}`, "user")
 }
@@ -13,14 +15,11 @@ const botReply = (botReply) => {
   showMessage(botReply, "bot");
 }
 
-const reason = () => {
-  inputWrapper.innerHTML  = `
-  <button class="send-btn" id="teeth-problem" type="submit">Problems with teeth</button>
+// this function is creating two buttons for second question
+ 
+// these two functions will check witch button is pressed in second question
 
-  <button class="send-btn" id="eye-problem" type="submit">Problems with eyes</button>
 
-  `
-}
 const clearInput = () =>  nameInput.value=""
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -49,23 +48,71 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
+  question  = 1;
   showMessage(`Hello there, What's your name?`, "bot");
 }
   // Just to check it out, change 'bot' to 'user' here 👆
+
+const questionSequence = (message) => {
+  if(question === 1) {
+    userReply();
+    selectProblem(message);
+  } else if (question === 2) {
+    showMessage(message, "user");
+    problemSolve(message);
+  }
+}
+
+// second question
+
+const selectProblem = () => {
+
+  question = 2;
+
+  botReply(`So ${nameInput.value}, what's the problem?`);
+
+  inputWrapper.innerHTML = `
+    <button id="eyeBtn">Sight problems</button>
+    <button id="teethBtn">Teeth problems</button>
+  `
+
+  document.getElementById("eyeBtn").addEventListener("click", () => {questionSequence("Sight problem")});
+  document.getElementById("teethBtn").addEventListener("click", () => {questionSequence("Teeth problem")});
+};
+
+  
+
+const problemSolve = (message) => {
+  question = 3;
+
+  botReply(`And what have happened to them?`);
+
+
+
+  if (message === "Teeth problem") {
+    inputWrapper.innerHTML = `
+    <button id="eyeBtn">Sensitite teeth</button>
+    <button id="teethBtn">Infected teeth</button>
+  ` 
+  } else if (message === "Sight problem") {
+    inputWrapper.innerHTML = `
+    <button id="eyeBtn">Sensitite eye</button>
+    <button id="teethBtn">Infected eye</button>
+  ` 
+  }
+};
 
 
   //EVENTLISTENER
     form.addEventListener("submit" , (event) => {
     event.preventDefault();
-    const inputValue = nameInput.value;
-    userReply();
-    clearInput();
-    botReply(`So ${inputValue}, what problems do you have?`)
-    reason();
+    questionSequence();
+
   })
 
 
-// Conditional
+// Question sequency
+
 
 // Timeout when website loads until first question
 setTimeout(greeting, 1000);
